e7813480ffbccb0ca136c96d225d310467249393
ljbin <ljbin1988@163.com>
2017-05-19 15:29:07 +0800

    udf程序更新

--- soft/GifCompression/main.cpp
+++ soft/GifCompression/main.cpp
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <errno.h> 
+#include <errno.h>
 
 #include <string>
 #include <string.h>
@@ -43,14 +43,14 @@ bool CompressGifGlobal(FIMULTIBITMAP *gif, int nFrames, int gap, const std::stri
 	delayVec.reserve(nFrames);
 
 	FIBITMAP *pFrame = NULL;
-	FITAG *tagDelay = NULL;
 	for (int i = 0; i < nFrames; i++)
 	{
 		pFrame = FreeImage_LockPage(gif, i);
-
+        //std::cout << "i = " << i << std::endl;
 		//"FrameTime" : 以毫秒计的每帧播放时间，所以在操作的时候是用ms作为单位
 		//而在最终保存文件的时候则是保存成百分之一秒，比如40ms保存成4cs，48ms也
 		//保存成4cs。而ImageMagick不管操作还是保存，都是用百分之一秒即cs来操作！
+		FITAG *tagDelay = NULL;
 		FreeImage_GetMetadata(FIMD_ANIMATION, pFrame, "FrameTime", &tagDelay);
 
 		if (tagDelay != NULL)
@@ -85,10 +85,10 @@ bool CompressGifGlobal(FIMULTIBITMAP *gif, int nFrames, int gap, const std::stri
 	{
 		int newNumFrames = (int)(nFrames / gap);
 
-		//normalize to 300ms if bigger than 300ms
-		if ((int)(totalDelay / newNumFrames) > 30)
+		//normalize to 200ms if bigger than 200ms
+		if ((int)(totalDelay / newNumFrames) > 20)
 		{
-			double scale = (newNumFrames * 1.0 * 30) / totalDelay;
+			double scale = (newNumFrames * 1.0 * 20) / totalDelay;
 			for (int i = 0; i < (int)delayVec.size(); i++)
 			{
 				delayVec[i] = (int)(delayVec[i] * scale);
@@ -149,7 +149,7 @@ bool CompressGifGlobal(FIMULTIBITMAP *gif, int nFrames, int gap, const std::stri
             if (execv(shellPath, paramsArr) < 0) //execv只有出错才有返回值
             {
                 std::cerr << "Failed to execute gifsicle-result!" << std::endl;
-				std::cerr << "ERROR: value = " << errno << ", it means : " << strerror(errno) << std::endl; 
+				std::cerr << "ERROR: value = " << errno << ", it means : " << strerror(errno) << std::endl;
                 //如果这个函数调用失败，我们最终目的应该是退出整个函数，返回false，代表未压缩成功
                 //因此这里如果执行失败，应该以一个错误状态退出，反馈给父进程，然后父进程捕捉到之后
                 //返回一个false，告诉整个函数结束了，没有压缩成功！
@@ -241,7 +241,15 @@ bool compressGif(std::string filePath)
 			{
 				FIBITMAP *palFrame0 = FreeImage_LockPage(gif, 0);
 				int nColors = FreeImage_GetColorsUsed(palFrame0);
-				FreeImage_UnlockPage(gif, palFrame0, FALSE);
+                FreeImage_UnlockPage(gif, palFrame0, FALSE);
+
+                //std::cout << "nColors = " << nColors << std::endl;
+                //2017.05.19修改记录：有些gif的local色盘如果都是256，不知道为什么colors设置成
+                //256之后会造成第一帧的delay变得非常大。而且256和128的显示效果差不多，所以设置成128
+                if (256 == nColors)
+                {
+                    nColors /= 2;
+                }
 
                 //remove local palette
                 std::vector<std::string> paramVec;
@@ -312,8 +320,10 @@ bool compressGif(std::string filePath)
                         return false;
                     }
 
+
                     //子进程正常退出，且返回值status不是5，则执行下面的代码
                     bool res = CompressGifGlobal(gif, nFrames, gap, filePath);
+
                     FreeImage_CloseMultiBitmap(gif, 0);
 
                     if (res == false)
@@ -380,7 +390,7 @@ int main(int argc, char *argv[])
 	//Step2: Compress gif
 	if (compressGif(strSrcPath))
 	{
-		std::cout << "Compressed!" << std::endl;
+		std::cout << "Compressed" << std::endl;
 	}
 	else
 	{
